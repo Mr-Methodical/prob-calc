aa7ead36231c40255a0a61f99309527bb17c056b
Ryan Howe <r3howe@uwaterloo.ca>
2026-09-15 13:21:07 -0400

    allowed program to be able to both take a name of the file for input or use standard input stream

--- Makefile
+++ Makefile
@@ -9,7 +9,11 @@ gcm.cache/iostream.stamp: Makefile
 	$(CXX) $(CXXFLAGS) -x c++-system-header iostream
 	touch gcm.cache/iostream.stamp
 
-dice: main.cpp gcm.cache/iostream.stamp Makefile
+gcm.cache/fstream.stamp: Makefile
+	$(CXX) $(CXXFLAGS) -x c++-system-header fstream
+	touch gcm.cache/fstream.stamp
+
+dice: main.cpp gcm.cache/iostream.stamp gcm.cache/fstream.stamp Makefile
 	$(CXX) $(CXXFLAGS) main.cpp -o dice
 
 clean:
--- main.cpp
+++ main.cpp
@@ -1,21 +1,72 @@
 #ifdef __INTELLISENSE__
 #include <iostream>
+#include <fstream>
 #else
 import <iostream>;
+import <fstream>;
 #endif
 
-int main() {
-  int sidesA = 0;
-  int sidesB = 0;
-  int sidesC = 0;
+struct Experiment {
+  int sidesA;
+  int sidesB;
+  int threshold;
+};
 
-  if (!(std::cin >> sidesA >> sidesB >> sidesC)) {
+// a reference to the input stream
+bool readExperiment(std::istream& in, Experiment& e) {
+  if (!(in >> e.sidesA >> e.sidesB >> e.threshold)) {
+    return false;
+  }
+  return true;
+}
+
+void printExperiment(std::ostream& out, const Experiment& e) {
+  out << "First die: " << e.sidesA << "\n";
+  out << "Second die: " << e.sidesB << "\n";
+  out << "Minimum sum: " << e.threshold << "\n";
+}
+
+int runExperiment(std::istream& in, std::ostream& out) {
+  // empty braces initialize all values to 0
+  Experiment experiment{};
+
+  if (!readExperiment(in, experiment)) {
     // ex. if they put in string instead of integer
-    std::cerr << "Expected 3 integers" << "\n";
+    std::cerr << "Expected 3 integers\n";
     return 1;
   }
 
-  std::cout << "First die: " << sidesA << "\n";
-  std::cout << "First die: " << sidesB << "\n";
-  std::cout << "First die: " << sidesC << "\n";
+  if ((experiment.sidesA < 1 || experiment.sidesA > 100) ||
+      (experiment.sidesB < 1 || experiment.sidesB > 100)) {
+    std::cerr << "Each die must have between 1 and 100 sides\n";
+    return 1;
+  }
+
+  printExperiment(out, experiment);
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    return runExperiment(std::cin, std::cout);
+  }
+
+  if (argc != 2) {
+    // square brackets means optional
+    std::cerr << "Usage: ./dice [input-file]\n";
+    return 1;
+  }
+
+  // now we know argc must be 2
+
+  // we do not need close file because of RAII where it will manage its own
+  // resources when it goes out of scope. Object's lifetime controls cleanup
+  std::ifstream file{argv[1]};
+
+  if (!file) {
+    std::cerr << "Cannot open " << argv[1] << "\n";
+    return 1;
+  }
+
+  return runExperiment(file, std::cout);
 }
